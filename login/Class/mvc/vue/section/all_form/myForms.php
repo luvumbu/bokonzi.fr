8823f0e69b7620f3aa84522404a162c6f9eba795
Luvumbu <luvumbu.n@gmail.com>
2020-06-12 07:44:32 +0200

    ajout des photos

--- login/Class/mvc/vue/section/all_form/myForms.php
+++ login/Class/mvc/vue/section/all_form/myForms.php
@@ -47,8 +47,7 @@ if ($result->num_rows > 0) {
  <?php  
  
  if($myform_source!="")
- {
-   
+ { 
    
    ?>
 
@@ -83,6 +82,7 @@ if ($result->num_rows > 0) {
  $titre_questions_id_  = $row["titre_questions_id"];
  $titre_questions_type = $row["titre_questions_type"] ;
  $titre_questions_id_myform = $row["titre_questions_id_myform"];
+ $titre_questions_source =$row["titre_questions_source"] ;
  ?>
 <form >
                 <div class="form-group" style="display: flex;justify-content: space-between;">             
@@ -90,6 +90,7 @@ if ($result->num_rows > 0) {
                   <div>
                     <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                         <div class="btn-group mr-2" role="group" aria-label="First group">                          
+                          
                           <button onclick="add_myForms(this)"   id="<?php echo  $titre_questions_id_ ?>"     title="<?php echo $titre_questions_id_.''?>" type="button" class="btn btn-secondary plus_" style="background-color:#44ba79"><i class="fa fa-plus-circle" ></i></i></button>                          
                           <button onclick="update_source(this)"   id="<?php echo  $titre_questions_id_.'2' ?>"   title="<?php echo $titre_questions_id_ .''?>"     type="button" class="btn btn-secondary square_"><i class="fa fa-check-square"></i></button>
                           <button onclick="update_source(this)"   id="<?php echo  $titre_questions_id_.'3' ?>"   title="<?php echo $titre_questions_id_ .''?>"     type="button" class="btn btn-secondary circle_"><i class="fa fa-dot-circle-o"></i></button>
@@ -102,6 +103,12 @@ if ($result->num_rows > 0) {
                 </div>                            
     </form> 
 <?php 
+if( $titre_questions_source!="")
+{
+ ?>
+  <img src="<?php echo $titre_questions_source.'.jpg' ?>" style="width:30%;margin-bottom:50px" alt="Responsive image">
+  <?php
+}
     // Create connection
 $connono = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -116,6 +123,7 @@ if ($resultono->num_rows > 0) {
   while($rowono = $resultono->fetch_assoc()) {
     $titres_data_id_ = $rowono["titres_data_id"];    
     $titres_data_titre= $rowono["titres_data_titre"];
+    $titres_data_source = $rowono["titres_data_source"];
     if($titre_questions_type=="square")
     {
       ?>
@@ -131,7 +139,16 @@ if ($resultono->num_rows > 0) {
                 <i class="fa fa-remove fa-remove1 remove_type_data" onclick="remove_form(this)" value="<?php echo   $titres_data_titre ?>"  title="<?php echo   $titres_data_id_ ?>"></i>
                  <button type="button"     title="<?php echo $titres_data_id_ ?>"     value="add_picture_data"  onclick="toggle_img_on(this)"          id="<?php echo $add_picture.$myform_id ?>"  class="btn btn-secondary form_img_all"><i class="fa fa-file-image-o"></i></button> 
           </div> 
+  
+
       <?php 
+      if($titres_data_source!="")
+      {
+        ?>
+        <img src="<?php echo $titres_data_source.'.jpg' ?>" style="width:15%;margin-bottom:50px" alt="Responsive image">
+        <?php 
+      }
+
     }
     else 
     {
@@ -144,12 +161,23 @@ if ($resultono->num_rows > 0) {
                 </div>
                 <input type="text" value="<?php echo   $titres_data_titre ?>"  title="<?php echo   $titres_data_id_ ?>"  onkeyup="update_source(this)" class="form-control titres_data" aria-label="Text input with radio" placeholder="Votre titre">               
               </div>
-          <div>
-                <i class="fa fa-remove fa-remove1 remove_type_data" onclick="remove_form(this)"  value="<?php echo   $titres_data_titre ?>"  title="<?php echo   $titres_data_id_ ?>"></i>
-                <button type="button"     title="<?php echo $titres_data_id_ ?>"     value="add_picture_data"  onclick="toggle_img_on(this)"          id="<?php echo $add_picture.$myform_id ?>"  class="btn btn-secondary form_img_all"><i class="fa fa-file-image-o"></i></button> 
 
-          </div> 
       <?php 
+            if($titres_data_source!="")
+            {
+              ?>
+              <img src="<?php echo $titres_data_source.'.jpg' ?>" style="width:15%;margin-bottom:50px" alt="Responsive image">
+             
+             <?php 
+            }
+       ?>     
+            <div>
+            <i class="fa fa-remove fa-remove1 remove_type_data" onclick="remove_form(this)"  value="<?php echo   $titres_data_titre ?>"  title="<?php echo   $titres_data_id_ ?>"></i>
+            <button type="button"     title="<?php echo $titres_data_id_ ?>"     value="add_picture_data"  onclick="toggle_img_on(this)"          id="<?php echo $add_picture.$myform_id ?>"  class="btn btn-secondary form_img_all"><i class="fa fa-file-image-o"></i></button> 
+      </div> 
+      <?php 
+
+
     }
   ?>
 
